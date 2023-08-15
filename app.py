d1a96fa61612fb409b5adc91cc629e9ff77f7176
Pavel <p.a.stepanovich@gmail.com>
2023-08-16 08:18:16 +1000

    Initial commit

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from streamlit_chat import message
+import pandas as pd
+import numpy as np
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    st.set_page_config(page_title="Help assistant")
+    st.title('Help assistant title')
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for mes in st.session_state.messages:
+        if mes["role"]=='assistant':
+            message(mes["content"])
+        elif mes["role"]=='user':
+            message(mes["content"], is_user=True)
+
+
+    # React to user input
+    if prompt := st.chat_input("What is up?"):
+
+        message(prompt, is_user=True)  # align's the message to the right
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        response = f"Echo: {prompt}"
+        message(response)  # align's the message to the right
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+if __name__ == '__main__':
+    main()
+
+
