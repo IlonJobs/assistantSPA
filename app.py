a092952990a3098b7c6059c3920bf5684087b511
Pavel <p.a.stepanovich@gmail.com>
2023-08-19 16:29:27 +1000

    add openai chat bot

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ import streamlit as st
 from streamlit_chat import message
 import pandas as pd
 import numpy as np
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
 
 # This is a sample Python script.
 
@@ -11,32 +17,49 @@ import numpy as np
 
 def main():
     st.set_page_config(page_title="Help assistant")
+    st.title('title')
     st.header('Help assistant header')
+
+    # Включение автоматической прокрутки
+    st.markdown("""
+        <style>
+            .message {
+                overflow-y: scroll !important;
+                max-height: 400px;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+
+    chat = ChatOpenAI(model_name='gpt-3.5-turbo',
+                      temperature=0)
 
     # Initialize chat history
     if "messages" not in st.session_state:
-        st.session_state.messages = []
+        st.session_state.messages = [SystemMessage(content='you are a polite assistant.')]
 
     # Display chat messages from history on app rerun
     for mes in st.session_state.messages:
-        if mes["role"]=='assistant':
-            message(mes["content"])
-        elif mes["role"]=='user':
-            message(mes["content"], is_user=True)
+        if mes.type=='human':
+            message(mes.content, is_user=True)
+        elif mes.type=='ai':
+            message(mes.content)
 
 
     # React to user input
-    if prompt := st.chat_input("What is up?"):
+    if user_question := st.chat_input("What is up?"):
 
-        message(prompt, is_user=True)  # align's the message to the right
+        message(user_question, is_user=True)  # align's the message to the right
         # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
+        # st.session_state.messages.append({"role": "user", "content": prompt})
+        st.session_state.messages.append(HumanMessage(content=user_question))
+        with st.spinner('Thinking...'):
+            response = chat(st.session_state.messages)
 
-        response = f"Echo: {prompt}"
-        message(response)  # align's the message to the right
+        message(response.content)  # align's the message to the right
         # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response})
-
+        # st.session_state.messages.append({"role": "assistant", "content": response})
+        st.session_state.messages.append(AIMessage(content=response.content))
+        # st.write(st.session_state.messages[1].type)
 if __name__ == '__main__':
     main()
 
