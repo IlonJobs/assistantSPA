ab102fe307f108917ba99ce2fd8fcd08401ca819
Pavel <p.a.stepanovich@gmail.com>
2023-09-18 09:09:06 +1000

    settings

--- settings_page.py
+++ settings_page.py
@@ -3,5 +3,5 @@ import streamlit as st
 def app():
     st.header('Settings page')
     st.markdown('---')
-    st.header('OpenAI')
-    st.header('Qdrant')
+    st.markdown('>OpenAI')
+    st.markdown('>Qdrant')
